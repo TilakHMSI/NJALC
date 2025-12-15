b64caf4233221639c4b776a7a1815239760681f4
Tilak <tilak.raj1@honda.hmsi.in>
2025-12-15 20:12:26 +0530

    confirm message size change

--- src/app/magic/VPRDO042DISPAF_OFF_ENTRY/CONFIRMATIONMESSAGE/Message.component.ts
+++ src/app/magic/VPRDO042DISPAF_OFF_ENTRY/CONFIRMATIONMESSAGE/Message.component.ts
@@ -28,8 +28,8 @@ export class Message extends TaskBaseMagicComponent implements MagicModalInterfa
     private static readonly showTitleBar: boolean = false;
     private static readonly x: number = 0;
     private static readonly y: number = 0;
-    private static readonly width: string = "572px";
-    private static readonly height: string = "272px";
+    private static readonly width: string = "595px";
+    private static readonly height: string = "260px";
     private static readonly isCenteredToWindow: boolean = true;
     private static readonly shouldCloseOnBackgroundClick: boolean = false;
     private static readonly isResizable: boolean = false;
