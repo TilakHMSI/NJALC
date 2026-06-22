17d4583f0fc467f79906081ab8111f4acd607772
Tilak <tilak.raj1@honda.hmsi.in>
2026-06-22 18:09:19 +0530

    PDI Success screen message size

--- src/app/magic/CONFIRMATION_MESSAGE_PDI/MESSAGE_PDI.component.ts
+++ src/app/magic/CONFIRMATION_MESSAGE_PDI/MESSAGE_PDI.component.ts
@@ -29,7 +29,7 @@ export class MESSAGE_PDI extends TaskBaseMagicComponent implements MagicModalInt
     private static readonly x: number = 0;
     private static readonly y: number = 0;
     private static readonly width: string = "580px";
-    private static readonly height: string = "262px";
+    private static readonly height: string = "305px";
     private static readonly isCenteredToWindow: boolean = true;
     private static readonly shouldCloseOnBackgroundClick: boolean = false;
     private static readonly isResizable: boolean = false;
